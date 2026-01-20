f03890efa2d95ebf5007197c2b6edc2d87d0b3c0
Fabio <fabio.vitoriano@icloud.com>
2026-01-20 09:41:33 +0000

    Criação da entrance activity.

---
+++ app/src/main/java/com/example/japonmarkey/EntranceActivity.java
@@ -0,0 +1,46 @@
+package com.example.japonmarkey;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class EntranceActivity extends AppCompatActivity {
+
+    // Definir uma var. de istância do tipo button
+    // Vai servir para manipular o Button
+
+    Button buttonToSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_entrance);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Aqui vamos colocar o eventlistener no btn login
+        // 1. Apanhar o componente no xml
+        this.buttonToSignUp = findViewById(R.id.buttonToSignUp);
+        // Aplicar o eventlistner
+        this.buttonToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(LoginActivity.this, "Cliquei no btn Login", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(EntranceActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
